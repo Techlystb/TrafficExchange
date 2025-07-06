7de0ec0a180fcee3ec08ffc446fbe21b526cd3a7
Techlystb blogs <techlystb@gmail.com>
2025-07-06 22:11:59 +0600

    Create firebase-config.js

---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB7qpEGqaVjQh-hyniCCbHh6Ut_fkInED4",
+  authDomain: "trafficexchange-stb.firebaseapp.com",
+  projectId: "trafficexchange-stb",
+  storageBucket: "trafficexchange-stb.appspot.com",
+  messagingSenderId: "69178339515",
+  appId: "1:69178339515:web:c3c5759f6fad673696f99e",
+  measurementId: "G-EW6RECN845"
+};
+
+firebase.initializeApp(firebaseConfig);
